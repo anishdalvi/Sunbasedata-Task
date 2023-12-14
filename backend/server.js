f5120a3080dbd8a541c7a05b4bba3fa0c7d95da3
anishDalvi <anishdalvi2001@gmail.com>
2023-12-14 16:06:52 +0530

    added path in backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@
 import express from "express";
 import cors from "cors";
 import apiRoutes from "./apiRoutes.js";
+import path from "path"
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +13,17 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use("/api", apiRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Not in Production"));
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
